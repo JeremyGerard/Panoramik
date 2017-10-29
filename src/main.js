b0e5eea2fdef90c9b29a93aa5ba8e3a83adbf8d6
JGERARD <jeremy.gerard@sword-group.com>
2017-10-29 17:27:05 +0100

    Creation page About

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueRouter from 'vue-router'
+
 
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
@@ -35,3 +35,36 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+//import the vue instance
+import Vue from 'vue'
+//import the App component
+import App from './App'
+//import the vue router
+import VueRouter from 'vue-router'
+//tell vue to use the router
+Vue.use(VueRouter)
+//define your routes
+const routes = []
+
+// Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    routes, // short for routes: routes
+    mode: 'history'
+})
+//instatinat the vue instance
+new Vue({
+//define the selector for the root component
+    el: '#app',
+    //pass the template to the root component
+    template: '<App/>',
+    //declare components that the root component can access
+    components: { App },
+    //pass in the router to the Vue instance
+    router
+}).$mount('#app')//mount the router on the app
