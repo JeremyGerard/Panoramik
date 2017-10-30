755fcbc7983c951ed22da7159d05118beb37a312
JGERARD <jeremy.gerard@sword-group.com>
2017-10-30 13:12:05 +0100

    Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,11 +1,15 @@
 <template>
     <div id="portfolio">
-        <b-col sm="6"><img v-img:my-group src="../assets/project/APS_Perspective.jpg"></b-col>
-        <b-col sm="6"><img v-img:my-group src="../assets/project/PERS COEUR ILOT.jpg"></b-col>
-        <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N1.jpg"></b-col>
-        <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N0.jpg"></b-col>
-        <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_DAY_04.jpg"></b-col>
-        <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_NIGHT_07.jpg"></b-col>
+        <b-container class="bv-example-row">
+            <b-row class="justify-content-md-center" align-v="center">
+                <b-col sm="6"><img v-img:my-group src="../assets/project/APS_Perspective.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/PERS COEUR ILOT.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N1.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N0.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_DAY_04.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_NIGHT_07.jpg"></b-col>
+            </b-row>
+        </b-container>
     </div>
 
 </template>
