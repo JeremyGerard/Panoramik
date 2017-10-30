e1e2e15ae31336909f38eba5c4b6015f16c71dd8
JGERARD <jeremy.gerard@sword-group.com>
2017-10-30 13:25:05 +0100

    Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,20 +2,28 @@
     <div id="portfolio">
         <b-container class="bv-example-row">
             <b-row class="justify-content-md-center" align-v="center">
-                <b-col sm="6">
-                    <b-img fluid alt="Responsive image"><img v-img:my-group src="../assets/project/APS_Perspective.jpg"></b-img></b-col>
-                <b-col sm="6">
-                    <img v-img:my-group src="../assets/project/PERS COEUR ILOT.jpg"></b-col>
-                <b-col sm="6">
-                    <img v-img:my-group src="../assets/project/NOVO_perspective_N1.jpg"></b-col>
-                <b-col sm="6">
-                    <img v-img:my-group src="../assets/project/NOVO_perspective_N0.jpg"></b-col>
-                <b-col sm="6">
-                    <img v-img:my-group src="../assets/project/BREST_RPA_RE_DAY_04.jpg"></b-col>
-                <b-col sm="6">
-                    <img v-img:my-group src="../assets/project/BREST_RPA_RE_NIGHT_07.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/APS_Perspective.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/PERS COEUR ILOT.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N1.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/NOVO_perspective_N0.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_DAY_04.jpg"></b-col>
+                <b-col sm="6"><img v-img:my-group src="../assets/project/BREST_RPA_RE_NIGHT_07.jpg"></b-col>
             </b-row>
         </b-container>
+
+        <div>
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/1/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/3/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/4/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/5/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/6/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/7/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/8/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/9/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+            <b-img-lazy src="https://lorempixel.com/600/400/cats/10/" center fluid-grow width="600" height="400" blank-color="#bbb" alt="img" class="my-5" />
+        </div>
+
+
     </div>
 
 </template>
