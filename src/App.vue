dbc439c1289d28b6531ebb65d94c5d3faa496420
JGERARD <jeremy.gerard@sword-group.com>
2017-11-09 12:50:37 +0100

    vue-masonry

--- src/App.vue
+++ src/App.vue
@@ -48,14 +48,62 @@
     }
 </script>
 
-<style scoped>
-    .header {
-        color: black;
-        font-family: Helvetica;
-        font-style: italic;
-        font-size: 16px;
+<style>
+    body {
+        margin: 0;
+    }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+    }
+    main {
         text-align: center;
-        margin-top: 60px;
+        margin-top: 40px;
+    }
+    header {
+        margin: 0;
+        height: 56px;
+        padding: 0 16px 0 24px;
+        background-color: #35495E;
+        color: #ffffff;
+    }
+    header span {
+        display: block;
+        position: relative;
+        font-size: 20px;
+        line-height: 1;
+        letter-spacing: .02em;
+        font-weight: 400;
+        box-sizing: border-box;
+        padding-top: 16px;
+    }
+    .cards {
+        column-count: 1;
+        column-gap: 1em;
+    }
+    .wrapper {
+        padding: 15px;
+    }
+    @media only screen and (min-width: 500px) {
+        .cards {
+            column-count: 2;
+        }
+    }
+    @media only screen and (min-width: 700px) {
+        .cards {
+            column-count: 3;
+        }
+    }
+    @media only screen and (min-width: 900px) {
+        .cards {
+            column-count: 4;
+        }
+    }
+    @media only screen and (min-width: 1100px) {
+        .cards {
+            column-count: 5;
+        }
     }
 </style>
-
